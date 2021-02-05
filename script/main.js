2624ed95018e7932a97b6c242306fb00918613a9
hari-hk <hariharakumar5196@gmail.com>
2021-02-05 22:28:32 +0530

    code clean up

--- script/main.js
+++ script/main.js
@@ -1,2 +1,6 @@
-const body = document.getElementsByTagName('body')[0];
-body.style.backgroundImage = `url("https://picsum.photos/${window.innerWidth}/${window.innerHeight}")`;
+function changeBackground() {
+    const body = document.getElementsByTagName('body')[0];
+    body.style.backgroundImage = `url("https://picsum.photos/${window.innerWidth}/${window.innerHeight}")`;
+}
+
+changeBackground();
