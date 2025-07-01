2234e4699515ffb1a36a79142fe53900b406cbad
hari hara kumar <hariharakumar5196@gmail.com>
2025-07-01 16:25:14 +0530

    delete unused files

--- script/main.js
+++
@@ -1,6 +0,0 @@
-function changeBackground() {
-    const body = document.getElementsByTagName('body')[0];
-    body.style.backgroundImage = `url("https://picsum.photos/${window.innerWidth}/${window.innerHeight}")`;
-}
-
-changeBackground();
